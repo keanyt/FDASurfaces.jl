75e1a9b80c64ea731cc5d8285679c02f32dc0357
Samuel Wu <cernunnos1710@gmail.com>
2023-08-24 18:18:06 +0800

    completed porting wasserstein

---
+++ src/fshapes/cuda/fshapes_distance/wasserstein/sinkhorn_gpu_conv_and_grad.jl
@@ -0,0 +1,165 @@
+include("sinkhorn_gpu_utils.jl")
+
+function sinkhorn_gpu_step(TYPE::Type, epsilon, lambda, weightGeom, weightGrass, 
+                            alpha_h, x_h, y_h, beta_h, mu_h, nu_h, gammax_h, gammay_h, gammaWd, gammadx_h, 
+                            dimPoint, dimVect, nx, ny, max_iter)
+    alpha_d = CUDA.fill(zero(TYPE), nx * dimVect)
+    x_d = CUDA.fill(zero(TYPE), nx * dimPoint)
+    y_d = CUDA.fill(zero(TYPE), ny * dimPoint)
+    beta_d = CUDA.fill(zero(TYPE), ny * dimVect)
+    mu_d = CUDA.fill(zero(TYPE), nx)
+    nu_d = CUDA.fill(zero(TYPE), ny)
+    gammax_d = CUDA.fill(zero(TYPE), nx * dimVect)
+    gammay_d = CUDA.fill(zero(TYPE), ny * dimVect)
+    gammadx_d = CUDA.fill(zero(TYPE), nx * dimPoint)
+
+    CUDA.copyto!(x_d, x_h)
+    CUDA.copyto!(y_d, y_h)
+    CUDA.copyto!(mu_d, mu_h)
+    CUDA.copyto!(nu_d, nu_h)
+
+    CUDA.copyto!(alpha_d, alpha_h)
+    CUDA.copyto!(beta_d, beta_h)
+
+    # compute on device
+    try
+        block_size_x = (192, 1, 1) # CUDA_BLOCK_SIZE in makefile_cuda.sh
+        grid_size_x = (div(nx + block_size_x[1] - 1, block_size_x[1]), 1, 1)
+
+        block_size_y = (192, 1, 1) # CUDA_BLOCK_SIZE in makefile_cuda.sh
+        grid_size_y = (div(ny + block_size_y[1] - 1, block_size_y[1]), 1, 1)
+
+        for iter = 1:max_iter
+            if dimpoint == 2 && dimVect == 1
+                @cuda threads=block_size_x blocks=grid_size_x sinkhorn_gpu_grad_conv_on_device(TYPE, 2, 1, epsilon, lambda, weightGeom, weightGrass, alpha_d, x_d, y_d, beta_d, mu_d, gammax_d, nx, ny)
+            elseif dimPoint == 4 && dimVect == 1
+                @cuda threads=block_size_x blocks=grid_size_x sinkhorn_gpu_grad_conv_on_device(TYPE, 4, 1, epsilon, lambda, weightGeom, weightGrass, alpha_d, x_d, y_d, beta_d, mu_d, gammax_d, nx, ny)
+            elseif dimPoint == 6 && dimVect == 1
+                @cuda threads=block_size_x blocks=grid_size_x sinkhorn_gpu_grad_conv_on_device(TYPE, 6, 1, epsilon, lambda, weightGeom, weightGrass, alpha_d, x_d, y_d, beta_d, mu_d, gammax_d, nx, ny)
+            else
+                println("sinkhorn_gpu_conv error: dimensions of Sinkhorn kernel not implemented in CUDA")
+                CUDA.unsafe_free!(gammadx_d)
+                CUDA.unsafe_free!(gammay_d)
+                CUDA.unsafe_free!(gammax_d)
+                CUDA.unsafe_free!(nu_d)
+                CUDA.unsafe_free!(mu_d)
+                CUDA.unsafe_free!(beta_d)
+                CUDA.unsafe_free!(y_d)
+                CUDA.unsafe_free!(x_d)
+                CUDA.unsafe_free!(alpha_d)
+                return -1
+            end
+
+            # update u
+            CUDA.copyto!(alpha_d, gammax_d)
+
+            if dimpoint == 2 && dimVect == 1
+                @cuda threads=block_size_y blocks=grid_size_y sinkhorn_gpu_grad_conv_on_device(TYPE, 2, 1, epsilon, lambda, weightGeom, weightGrass, beta_d, y_d, x_d, alpha_d, nu_d, gammay_d, ny, nx)
+            elseif dimpoint == 4 && dimVect == 1
+                @cuda threads=block_size_y blocks=grid_size_y sinkhorn_gpu_grad_conv_on_device(TYPE, 4, 1, epsilon, lambda, weightGeom, weightGrass, beta_d, y_d, x_d, alpha_d, nu_d, gammay_d, ny, nx) 
+            elseif dimpoint == 6 && dimVect == 1
+                @cuda threads=block_size_y blocks=grid_size_y sinkhorn_gpu_grad_conv_on_device(TYPE, 6, 1, epsilon, lambda, weightGeom, weightGrass, beta_d, y_d, x_d, alpha_d, nu_d, gammay_d, ny, nx)
+            else
+                println("sinkhorn_gpu_conv error: dimensions of sinkhorn_gpu_grad_conv_on_device kernel not implemented in CUDA")
+                CUDA.unsafe_free!(gammadx_d)
+                CUDA.unsafe_free!(gammay_d)
+                CUDA.unsafe_free!(gammax_d)
+                CUDA.unsafe_free!(nu_d)
+                CUDA.unsafe_free!(mu_d)
+                CUDA.unsafe_free!(beta_d)
+                CUDA.unsafe_free!(y_d)
+                CUDA.unsafe_free!(x_d)
+                CUDA.unsafe_free!(alpha_d)
+                return -1
+            end
+            
+            # update v
+            CUDA.copyto!(beta_d, gammay_d)
+        end
+
+        # block until the device has completed
+        CUDA.synchronize()
+
+        # send data from device to host
+        CUDA.copyto!(gammax_h, alpha_d)
+        CUDA.copyto!(gammay_h, beta_d)
+
+        if dimpoint == 2 && dimVect == 1
+            @cuda threads=block_size_y blocks=grid_size_y grad_xW_on_device(TYPE, 2, 1, epsilon, weightGeom, weightGrass, alpha_d, x_d, y_d, beta_d, gammadx_d, nx, ny)
+        elseif dimpoint == 4 && dimVect == 1
+            @cuda threads=block_size_y blocks=grid_size_y grad_xW_on_device(TYPE, 4, 1, epsilon, weightGeom, weightGrass, alpha_d, x_d, y_d, beta_d, gammadx_d, nx, ny)
+        elseif dimpoint == 6 && dimVect == 1
+            @cuda threads=block_size_y blocks=grid_size_y grad_xW_on_device(TYPE, 6, 1, epsilon, weightGeom, weightGrass, alpha_d, x_d, y_d, beta_d, gammadx_d, nx, ny)
+        else
+            println("sinkhorn_gpu_conv error: dimensions of grad_xW_on_device kernel not implemented in CUDA")
+            CUDA.unsafe_free!(gammadx_d)
+            CUDA.unsafe_free!(gammay_d)
+            CUDA.unsafe_free!(gammax_d)
+            CUDA.unsafe_free!(nu_d)
+            CUDA.unsafe_free!(mu_d)
+            CUDA.unsafe_free!(beta_d)
+            CUDA.unsafe_free!(y_d)
+            CUDA.unsafe_free!(x_d)
+            CUDA.unsafe_free!(alpha_d)
+            return -1
+        end
+
+        # block until the device has completed
+        CUDA.synchronize()
+
+        # send data from device to host
+        CUDA.copyto!(gammadx_h, gammadx_d)
+
+        # compute dual energy values
+        dotpumu = dot(gammax_h, mu_h)
+        dotpvnu = dot(gammay_h, nu_h)
+        total_mass = 0.0
+
+        if dimpoint == 2 && dimVect == 1
+            @cuda threads=block_size_y blocks=grid_size_y Wdual_on_device(TYPE, 2, 1, epsilon, weightGeom, weightGrass, alpha_d, x_d, y_d, beta_d, gammadx_d, nx, ny)
+        elseif dimpoint == 4 && dimVect == 1
+            @cuda threads=block_size_y blocks=grid_size_y Wdual_on_device(TYPE, 4, 1, epsilon, weightGeom, weightGrass, alpha_d, x_d, y_d, beta_d, gammadx_d, nx, ny)
+        elseif dimpoint == 6 && dimVect == 1
+            @cuda threads=block_size_y blocks=grid_size_y Wdual_on_device(TYPE, 6, 1, epsilon, weightGeom, weightGrass, alpha_d, x_d, y_d, beta_d, gammadx_d, nx, ny)
+        else
+            println("sinkhorn_gpu_conv error: dimensions of Wdual kernel not implemented in CUDA")
+            CUDA.unsafe_free!(gammadx_d)
+            CUDA.unsafe_free!(gammay_d)
+            CUDA.unsafe_free!(gammax_d)
+            CUDA.unsafe_free!(nu_d)
+            CUDA.unsafe_free!(mu_d)
+            CUDA.unsafe_free!(beta_d)
+            CUDA.unsafe_free!(y_d)
+            CUDA.unsafe_free!(x_d)
+            CUDA.unsafe_free!(alpha_d)
+            return -1
+        end
+
+        # block until the device has completed
+        CUDA.synchronize()
+
+        # send data from device to host
+        CUDA.copyto!(Gammax_h, gammax_d)
+
+        for i = 1:nx
+            total_mass += Gammax_h[i]
+        end
+        
+        # compute the energy
+        # not sure if this actually performs deep copy??
+        copy!(total_mass + dotpvnu + dotpumu, gammaWd)
+    
+    # ensures memory is freed in case of thrown exception
+    finally
+        CUDA.unsafe_free!(gammadx_d)
+        CUDA.unsafe_free!(gammay_d)
+        CUDA.unsafe_free!(gammax_d)
+        CUDA.unsafe_free!(nu_d)
+        CUDA.unsafe_free!(mu_d)
+        CUDA.unsafe_free!(beta_d)
+        CUDA.unsafe_free!(y_d)
+        CUDA.unsafe_free!(x_d)
+        CUDA.unsafe_free!(alpha_d)
+    end
+    return 0
+end
